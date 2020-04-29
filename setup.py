9db43ba97498ecc6617f18cb19cac5054ff6726f
rkeene <rkeene@noemail.net>
2020-04-29 00:34:16 +0000

    limitlessled_rf v0.1
    FossilOrigin-Name: 1e49925e09881daddb17614381f73c6b294a58428cc90e54832df0b0197e665f

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="limitlessled_rf",
+	version="0.1",
+	author="Roy Keene",
+	author_email="pypi@rkeene.org",
+	description="Python LimitlessLED via RF",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://chiselapp.com/user/rkeene/repository/limitlessled_rf/",
+	packages=["limitlessled_rf"],
+	package_dir={"":"."},
+	license="MIT License",
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6'
+)
