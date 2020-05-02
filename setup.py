46042162e6ec52fe06477c3f067ca8e88138816b
rkeene <rkeene@noemail.net>
2020-05-02 15:11:52 +0000

    limitlessled_rf v0.10
    FossilOrigin-Name: abd184fa6363655723dfc26968c2199ad6053dd401d9d842e4a2c4eb5805fa21

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
 	name="limitlessled_rf",
-	version="0.9",
+	version="0.10",
 	author="Roy Keene",
 	author_email="pypi@rkeene.org",
 	description="Python LimitlessLED via RF",
